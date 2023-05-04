6552ce0d970940c5cf1894728e9fa15a2ed0ed4f
maris333 <animaral@gmail.com>
2023-05-04 14:20:25 +0200

    Fix

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ from timeit import default_timer
 
 
 def logged(func):
-    def wrapper(*args):
-        result = func(*args)
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
         print(f"You called {func.__name__}{args}. It returned {result}.")
         return result
 
@@ -13,9 +14,9 @@ def logged(func):
 
 def starred(func):
     @wraps(func)
-    def wrapper():
+    def wrapper(*args, **kwargs):
         print("************")
-        func()
+        func(*args, **kwargs)
         print("************")
     return wrapper
 
@@ -24,35 +25,35 @@ def count(func):
     dictionary = {func.__name__: 0}
 
     @wraps(func)
-    def wrapper():
+    def wrapper(*args, **kwargs):
         dictionary[func.__name__] += 1
         for key, value in dictionary.items():
             if value == 1:
                 print(f"Function {key} was called {value} time.")
             else:
                 print(f"Function {key} was called {value} times.")
-        return func()
+        return func(*args, **kwargs)
     return wrapper
 
 
 def arg_check(expected_type):
     def check(func):
         @wraps(func)
-        def wrapper(arg):
-            if isinstance(arg, expected_type):
-                print(f"Expected type {expected_type} and got type {type(arg)}.")
-                return func(arg)
+        def wrapper(*args, **kwargs):
+            if isinstance(*args, expected_type):
+                print(f"Expected type {expected_type} and got type {type(*args)}.")
+                return func(*args, **kwargs)
             else:
-                raise TypeError(f"Expected type {expected_type} but got type {type(arg)}.")
+                raise TypeError(f"Expected type {expected_type} but got type {type(*args)}.")
         return wrapper
     return check
 
 
 def time_this(func):
     @wraps(func)
-    def wrapper(*args):
+    def wrapper(*args, **kwargs):
         start = default_timer()
-        func(*args)
+        func(*args, **kwargs)
         stop = default_timer()
         duration = stop - start
         print(f"Function {func.__name__} lasted {duration} sec.")
